f3b6dc951b9d08f393a41a34ddc139fc891e7f77
William Marstrand <wdmdevelop@gmail.com>
2022-03-30 16:58:29 +0200

    Working on making DNN predictions correct

--- exercises/week8/DNN.py
+++ exercises/week8/DNN.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-EPS = np.finfo(float).eps
+EPS = 10**(-6)
 
 class ThreeHiddenDNN():
     def __init__(self, step_size=0.05) -> None:
         # Initialize random weights
         self.step_size = step_size
-        self.weights = np.asarray([
-            np.random.randn(3,3),
-            np.random.randn(4,2)
-        ])
-
+        self.W1 = np.random.randn(3,3)*(1/np.sqrt(3))
+        self.W2 = np.random.randn(4,2)*(1/np.sqrt(4))
 
     def backpropagation(self, x, y_pred, y_true, i):
         '''
@@ -32,17 +29,28 @@ class ThreeHiddenDNN():
         # Derivatives of activation
         a_prime = (h > 0).astype(int).T
         # Weights without bias row (last row)
-        W_hat = self.weights[1][:-1]
+        W_hat = self.W2[:-1]
         W_hat[:,0] = a_prime * W_hat[:,0]
         W_hat[:,1] = a_prime * W_hat[:,1]
+        W_hat = (1./x.shape[0])*W_hat
         
         # Update values for weights[0]
         delta_1 = W_hat @ delta_2.T
         # Partial derivatives for updating weights[0]
         Q0 = np.outer(np.append(x, 1).T, delta_1)
 
-        self.weights[0] -= self.step_size*Q0 + EPS
-        self.weights[1] -= self.step_size*Q1 + EPS
+        self.W1 -= self.step_size*Q0
+        self.W2 -= self.step_size*Q1
+
+    def softmax(self, y_hat):
+        '''
+            Softmax to perform classification
+        '''
+        y_hat_exp = np.exp(y_hat)
+        y_hat_sum = np.sum(y_hat, axis=1)
+        y_hat_sum = np.reshape(y_hat_sum, (y_hat_sum.shape[0],1))
+        y_hat_softmax = y_hat_exp/y_hat_sum
+        return y_hat_softmax
 
     def forward(self, x):
         ''' 
@@ -55,11 +63,13 @@ class ThreeHiddenDNN():
 
         :returns:   The predicted labels
         '''
-        z = np.c_[x, np.ones((x.shape[0],1))]@self.weights[0]
-        # Node values between hidden layer and output
+        z = np.c_[x, np.ones((x.shape[0],1))]@self.W1
+        # Node values between hidden layer and output, ReLU
         self.h = np.maximum(z, 0)
-        y_hat = np.c_[self.h, np.ones((self.h.shape[0],1))]@self.weights[1]
-        # Softmax to perform classification
-        y = np.exp(y_hat)/np.sum(np.exp(y_hat), axis=1, keepdims=True)
+        y_hat = np.c_[self.h, np.ones((x.shape[0],1))]@self.W2
+        # Using subtraction of np.max to avoid overflow, Goodfellow: Deep Learning page 81
+        max_y_hat = np.max(y_hat, axis=1, keepdims=True)
+        new_y_hat = y_hat-max_y_hat
+        y = self.softmax(new_y_hat)
 
         return y
